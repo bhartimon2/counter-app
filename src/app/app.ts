aaf9a11001121a31e20d39893e1fa639b6e19891
Hariom <hbharti156@gmail.com>
2025-07-30 21:44:49 +0530

    Initial changes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,65 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
+interface LogEntry {
+  action: string;    
+  counter: number; 
+}
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  standalone: true,
+  imports: [CommonModule],
+  styleUrls: ['./app.css']
 })
 export class App {
-  protected readonly title = signal('counter-project');
+  counter = 0;
+  private resetLoggedAtZero = false;
+
+
+  log: LogEntry[] = [];
+
+  animateCounter() {
+    const counterElem = document.querySelector('.counter') as HTMLElement;
+    if (!counterElem) return;
+  
+    counterElem.classList.remove('animate');
+  
+    void counterElem.offsetWidth;
+  
+    counterElem.classList.add('animate');
+  }
+  
+
+  increment() {
+    this.counter++;
+    this.animateCounter();
+    this.log.push({ action: '+1', counter: this.counter });
+  }
+
+  decrement() {
+    this.counter--;
+    this.animateCounter();
+
+    this.log.push({ action: '-1', counter: this.counter });
+  }
+
+  reset() {
+    this.animateCounter();
+
+    if (this.counter !== 0) {
+      this.counter = 0;
+      this.resetLoggedAtZero = true;
+      this.log.push({ action: 'Reset', counter: this.counter });
+    } else if (!this.resetLoggedAtZero) {
+      this.resetLoggedAtZero = true;
+      this.log.push({ action: 'Reset', counter: this.counter });
+    }
+  }
+
+  clearHistory() {
+    this.animateCounter();
+    this.log = [];
+    this.resetLoggedAtZero = false; 
+  }
 }
